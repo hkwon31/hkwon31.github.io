df59ce2f6ee5299fd3d768b544b28d61423cc663
hkwon31 <hkwon31@uic.edu>
2021-03-03 10:20:06 -0600

    0303 second

--- personal_webpage/responsive_page/custom.js
+++ personal_webpage/responsive_page/custom.js
@@ -7,4 +7,16 @@ $(function(){
     $('.trigger').removeClass('active');
     $('.gnb').removeClass('active');
   })
+  //  Smooth Scrolling
+  $('.menu a').click(function(e){
+    $.scrollTo(this.hash || 0, 900);
+  })
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 50) {
+      $('header').addClass('active');
+    }
+    else {
+      $('header').removeClass('active');
+    }
+  })
 })
