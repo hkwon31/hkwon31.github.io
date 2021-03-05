1ff89d84e6cf851ae3f97a8f3cd5bd6bed8c1253
hkwon31 <hkwon31@uic.edu>
2021-03-05 10:21:18 -0600

    slider section

--- personal_webpage/responsive_page/custom.js
+++ personal_webpage/responsive_page/custom.js
@@ -19,4 +19,11 @@ $(function(){
       $('header, .gototop').removeClass('active');
     }
   })
+  $('.history-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  })
 })
